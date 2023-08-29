ab137ece1608bdb06fe7a519c9794a6528a56ced
Ganesh  Shinde <ganesh.shinde@splus.local>
2023-08-29 18:08:19 +0530

    update for testing

--- controllers/name.controller.js
+++ controllers/name.controller.js
@@ -13,7 +13,7 @@ const _ = require("lodash");
 require("dotenv").config();
 const s3 = new aws.S3();
 
-// create a Name
+// create a Name ak
 exports.getNames = async(req, res) => {
   try {
       const name  = req.body.name;
